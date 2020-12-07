980b60be73a9eaffe0b9919e97e1eb6ca2ca9f8d
jwd1120 <jwd1120@atl42jwd082723.lan>
2020-12-06 19:11:14 -0500

    initial commit

--- src/App.js
+++ src/App.js
@@ -9,6 +9,9 @@ function App() {
         <p>
           Edit <code>src/App.js</code> and save to reload.
         </p>
+        <p>
+          Hola amigos!
+        </p>
         <a
           className="App-link"
           href="https://reactjs.org"
